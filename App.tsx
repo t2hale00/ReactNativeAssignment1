d89f047678a8962dce80aaea9e3a16b255ecc5ff
Lea <112546991+t2hale00@users.noreply.github.com>
2024-01-16 20:53:11 +0200

    Codes without colored button added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -18,13 +18,24 @@ export default function App() {
             source={require('./images/faceimage.jpeg')}
           />
           <View style={styles.nameEmailContainer}>
-            <Text style={[styles.nameText, styles.boldText]}>John Crawford</Text>
-            <Text style={styles.emailText}>John123@gmail.com</Text>
+            <Text style={[styles.nameText, styles.boldText]}>Mary Crawford</Text>
+            <Text style={styles.emailText}>Mary123@gmail.com</Text>
           </View>
         </View>
 
         <View style={styles.separator}></View>
-        <Text style={styles.transactionTimeText}>Transaction Time</Text>
+        <Text style={styles.transactionTimeText}>Transaction done on 12 January 2018</Text>
+        <Text style={styles.transactionTimeText}>Your reference number is 03492390</Text>
+
+        {/* Continue Button */}
+        <TouchableOpacity
+          style={styles.continueButton}
+          onPress={() => {
+            // Add your continue button functionality here
+          }}
+        >
+          <Text style={styles.continueButtonText}>Continue</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -39,7 +50,7 @@ const styles = StyleSheet.create({
   },
   box: {
     width: 330,
-    height: 500,
+    height: 600,
     backgroundColor: 'white',
     alignItems: 'center',
     paddingVertical: 20,
@@ -48,42 +59,46 @@ const styles = StyleSheet.create({
   image1: {
     width: 100,
     height: 100,
+    marginTop: 60,
   },
   successText: {
     marginTop: 10,
     fontSize: 16,
   },
   amountText: {
-    fontSize: 18,
+    fontSize: 30,
     fontWeight: 'bold',
     color: '#356154',
-    marginTop: 10,
+    marginTop: 20,
+    marginBottom: 10,
   },
   profileContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 10,
+    marginBottom: 20,
   },
   image2: {
-    width: 80,
-    height: 80,
+    width: 60,
+    height: 60,
     borderRadius: 40,
   },
   nameEmailContainer: {
-    marginLeft: 10,
+    marginLeft: 20,
   },
   nameText: {
-    fontSize: 16,
+    fontSize: 20,
   },
   emailText: {
-    fontSize: 14,
+    fontSize: 12,
     color: '#555',
   },
   separator: {
     height: 1,
-    width: '100%',
+    width: '90%',
     backgroundColor: '#ccc',
     marginVertical: 10,
+    marginBottom: 30,
   },
   transactionTimeText: {
     fontSize: 14,
@@ -91,5 +106,20 @@ const styles = StyleSheet.create({
   },
   boldText: {
     fontWeight: 'bold',
+    fontSize: 16,
+  },
+ 
+  continueButton: {
+    backgroundColor:  '#356154',
+    padding: 15,
+    borderRadius: 30,
+    marginTop: 60,
+    width: '90%',
+  },
+  continueButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
